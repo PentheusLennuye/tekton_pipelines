abe14e52e4c9de8811e7d24617a1331aeb8cf018
PentheusLennuye <gmc@cummings-online.ca>
2023-11-07 16:32:51 -0500

    clean up has different triggers

--- common/cleanup.sh
+++ common/cleanup.sh
@@ -9,6 +9,6 @@ kubectl delete -k overlays/develop
 
 kubectl delete pipelineruns -n develop -l triggers.tekton.dev/trigger=trigger-push
 kubectl delete pipelineruns -n develop -l triggers.tekton.dev/trigger=trigger-pr
-kubectl delete pipelineruns -n develop -l triggers.tekton.dev/trigger=trigger-close
+kubectl delete pipelineruns -n develop -l triggers.tekton.dev/trigger=trigger-merge
 kubectl delete -k overlays/develop
 kubectl get pv | grep develop-nfs-client | awk '{print $1}' | xargs kubectl delete pv
