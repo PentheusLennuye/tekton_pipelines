0a70cc9e1e9c245c8e5fa7d338a6b9c58b88f7fd
PentheusLennuye <gmc@cummings-online.ca>
2023-11-07 11:51:20 -0500

    All works

--- common/cleanup.sh
+++ common/cleanup.sh
@@ -3,3 +3,12 @@
 # Before shutting down K8S, lose the old pods
 
 kubectl delete -k overlays/develop
+#!/usr/bin/sh
+# Cleaning Up
+# Before shutting down K8S, lose the old pods
+
+kubectl delete pipelineruns -n develop -l triggers.tekton.dev/trigger=trigger-push
+kubectl delete pipelineruns -n develop -l triggers.tekton.dev/trigger=trigger-pr
+kubectl delete pipelineruns -n develop -l triggers.tekton.dev/trigger=trigger-close
+kubectl delete -k overlays/develop
+kubectl get pv | grep develop-nfs-client | awk '{print $1}' | xargs kubectl delete pv
--- helloweb/cleanup.sh
+++ helloweb/cleanup.sh
@@ -2,8 +2,7 @@
 # Cleaning Up
 # Before shutting down K8S, lose the old pods
 
-kubectl delete pipelineruns -n develop -l tekton.dev/pipeline=pl-helloworld-open
-kubectl delete pipelineruns -n develop -l tekton.dev/pipeline=pl-helloworld-close
-kubectl delete pipelineruns -n develop -l triggers.tekton.dev/trigger=trigger-pr
-kubectl delete -k overlays/develop
+kubectl delete pipelineruns -n develop -l tekton.dev/pipeline=pl-github-deploy
+kubectl delete pipelineruns -n develop -l tekton.dev/pipeline=pl-remove-deployment
+#kubectl delete -k overlays/develop
 kubectl get pv | grep develop-nfs-client | awk '{print $1}' | xargs kubectl delete pv
