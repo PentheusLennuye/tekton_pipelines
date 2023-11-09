976a4f19b39cde6ef8ea93439c929a0b28388fa3
PentheusLennuye <gmc@cummings-online.ca>
2023-11-09 15:07:39 -0500

    Added tagging

--- common/cleanup.sh
+++ common/cleanup.sh
@@ -2,11 +2,6 @@
 # Cleaning Up
 # Before shutting down K8S, lose the old pods
 
-kubectl delete -k overlays/develop
-#!/usr/bin/sh
-# Cleaning Up
-# Before shutting down K8S, lose the old pods
-
 kubectl delete pipelineruns -n develop -l triggers.tekton.dev/trigger=trigger-push
 kubectl delete pipelineruns -n develop -l triggers.tekton.dev/trigger=trigger-pr
 kubectl delete pipelineruns -n develop -l triggers.tekton.dev/trigger=trigger-merge
